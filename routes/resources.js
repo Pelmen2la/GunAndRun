8adf4495233587279e63f06ffda6b046eb3da4c3
Pelmen2la <pelmengi12>
2016-08-15 11:48:06 +0300

    If country has no icons returns Unknown.png icon

--- routes/resources.js
+++ routes/resources.js
@@ -1,6 +1,10 @@
 'use strict';
 
-var utils = require('app/utils');
+var utils = require('app/utils'),
+    fs = require('fs'),
+    path = require('path'),
+    appDir = path.dirname(require.main.filename),
+    SMALL_FLASGS_ICONS_PATH = '/public/resources/icons/flags/small/'
 
 module.exports = function(app) {
     app.get('/country_list/', function(req, res, next) {
@@ -9,6 +13,16 @@ module.exports = function(app) {
                     success: true,
                     countryList: countryList
             });
+        });
+    });
+
+    app.get('/resources/icons/flags/small/:fileName', function(req, res, next) {
+        fs.stat(path.join(appDir, SMALL_FLASGS_ICONS_PATH, req.params.fileName), function(err, stat) {
+            if(err == null) {
+                next();
+            } else {
+                res.sendFile(path.join(appDir, SMALL_FLASGS_ICONS_PATH, 'Unknown.png'));
+            }
         });
     });
 };
